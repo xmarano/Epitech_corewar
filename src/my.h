2bbc227312a284dc1162134b11b63773410adae4
xmarano <leo.gregori@epitech.eu>
2024-04-30 23:37:17 +0200

    [ADD] : New function lib

--- src/my.h
+++ src/my.h
@@ -16,12 +16,14 @@
     #include <sys/stat.h>
     #include <stdbool.h>
     #include <errno.h>
+    #include <stdint.h>
 char *my_strchr(const char *s, int c);
 char *my_strstr(char *source, char *search);
 char *my_strrchr(const char *s, int c);
 int my_put_nbr(int nb);
 int my_atoi(char *str);
 char my_putchar(char c);
+int my_is_num(char *str);
 int my_strlen(char const *str);
 int my_putstr(char const *str);
 int my_put_nbr_recursive(int nb);
